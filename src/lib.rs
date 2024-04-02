dbf7c39997c247a8c14c5b6dcc6d58f0dae18939
Tobie Morgan Hitchcock <tobie@surrealdb.com>
2024-04-02 23:19:37 +0100

    Ensure clippy checks pass

--- src/lib.rs
+++ src/lib.rs
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#![allow(clippy::bool_comparison)]
+
 pub mod db;
 pub mod err;
 pub mod kv;
